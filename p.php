653834988479663f85609c326f144fe6128d3b3d
webnet <muhommadraahim1@gmail.com>
2019-08-16 13:24:48 +0500

    Add files via upload

---
+++ p.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<center><br><br><form action="" method="GET">
+<input type="text" name="w" placeholder="ip / web address">
+<input type="text" name="p" placeholder="ports">
+<input type="submit" value="check">
+</form>
+<?php
+error_reporting(0);
+$w = $_GET['w'];
+$p = $_GET['p'];;
+if(fsockopen($w,$p))
+{
+echo "I can see port <font color='green'>$p</font> on web <font color='green'>$w</font>";
+}
+else
+{
+echo "I cannot see port <font color='red'>$p</font> on web <font color='red'>$w</font>";
+}
+?>
